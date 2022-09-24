de3844bdc83a67dfef7d32d213d6f4c13cbe5805
ew12 <ew12@users.noreply.github.com>
2022-09-25 07:28:43 +0800

    Update SyfywireBridge.php

--- bridges/SyfywireBridge.php
+++ bridges/SyfywireBridge.php
@@ -1,24 +1,24 @@
 <?php
 class SyfywireBridge extends BridgeAbstract {
-  const NAME = 'syfy-wire';
-  const URI = 'https://www.syfy.com/syfy-wire';
-  const DESCRIPTION = 'syfy.com used to have a rss feed, but that feed is almost 1 year dead now(202209). So here we go.';
-  const CACHE_TIMEOUT = 3600;
-  
-  public function collectData() {
-    $html = getSimpleHTMLDOM ( self::URI );
-    
-    foreach ( $html->find ( '.card-feed article' ) as $article ) {
-      $item = [];
-      if ( $article->hasClass ( 'promo' ) === false ) {
-        $item['enclosures'] = $article->find('img', 0)->getAttribute('src');
-        $item['uri'] = $article->find ( 'a', 0 ) ->href;
-        $item['title'] = $article->find ( 'h2.headline', 0 )->innertext() ;
-        $item['timestamp'] = $article->find( '.date', 0 )->getAttribute ( 'datetime' );
-        $item['author'] = $article->find ( '.author-name', 0 )->innertext() ;
-        $item['content'] = $article->find ( '.description' , 0 )->innertext() ;
-        $this->items[] = $item; 
-      }
-    }
-  }
+	const NAME = 'syfy-wire';
+	const URI = 'https://www.syfy.com/syfy-wire';
+	const DESCRIPTION = 'syfy.com used to have a rss feed, but that feed is almost 1 year dead now(202209). So here we go.';
+	const CACHE_TIMEOUT = 3600;
+
+	public function collectData() {
+		$html = getSimpleHTMLDOM ( self::URI );
+		
+		foreach ( $html->find ( '.card-feed article' ) as $article ) {
+			$item = [];
+			if ( $article->hasClass ( 'promo' ) === false ) {
+				$item['enclosures'] = array ( self::uri.$article->find('img', 0)->getAttribute('src') );
+				$item['uri'] = $article->find ( 'a', 0 ) ->href;
+				$item['title'] = $article->find ( 'h2.headline', 0 )->innertext();
+				$item['timestamp'] = $article->find( '.date', 0 )->getAttribute ( 'datetime' );
+				$item['author'] = $article->find ( '.author-name', 0 )->innertext();
+				$item['content'] = $article->find ( '.description' , 0 )->innertext();
+				$this->items[] = $item;
+			}
+		}
+	}
 }
