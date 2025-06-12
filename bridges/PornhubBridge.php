b8019eeadda5d1c1a13ed4d9b93a337cfd13ba17
ew12 <ew12@users.noreply.github.com>
2025-06-13 01:27:51 +0800

    Update PornhubBridge.php

--- bridges/PornhubBridge.php
+++ bridges/PornhubBridge.php
@@ -71,7 +71,7 @@ class PornhubBridge extends BridgeAbstract
             'cookie: accessAgeDisclaimerPH=1'
         ]);
 
-        foreach ($html->find('div.videoUList ul.videos li.videoblock') as $element) {
+        foreach ($html->find('div.videoUList',0)->find('ul.videos li.videoblock') as $element) {
             $item = [];
 
             $item['author'] = $this->getInput('q');
