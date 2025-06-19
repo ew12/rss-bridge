38c392d3a0937c1e373a942725ddfcb7da64e370
ew12 <ew12@users.noreply.github.com>
2025-06-19 14:24:19 +0800

    Update PornhubBridge.php

--- bridges/PornhubBridge.php
+++ bridges/PornhubBridge.php
@@ -71,7 +71,7 @@ class PornhubBridge extends BridgeAbstract
             'cookie: accessAgeDisclaimerPH=1'
         ]);
 
-        if ( $html->find('div.latestThumbDesign',0)->find('ul.videos li.videoblock') === NULL ) {
+        if ( $html->find('div.latestThumbDesign') === NULL ) {
             $this->items[] = array ( 'author'=>$this->getInput ( 'q' ) , 'title'=>'no content' , 'uri'=>$uri , 'content'=>'no content' , 'timestamp'=>'0' );
         }
 
